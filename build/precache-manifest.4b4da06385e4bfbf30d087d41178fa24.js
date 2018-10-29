1ff9420172b01242e35b6dc9e95c860ed731702c
Joshua Min <minje@usc.edu>
2018-10-28 19:52:43 -0700

    build

---
+++ build/precache-manifest.4b4da06385e4bfbf30d087d41178fa24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e4ca40ed45b0502ef57f",
+    "url": "/static/js/main.e4ca40ed.chunk.js"
+  },
+  {
+    "revision": "9d8916d9358f41d3d171",
+    "url": "/static/js/1.9d8916d9.chunk.js"
+  },
+  {
+    "revision": "e4ca40ed45b0502ef57f",
+    "url": "/static/css/main.e0fce55b.chunk.css"
+  },
+  {
+    "revision": "d1b82715f6d13d945afc936295a8c5db",
+    "url": "/index.html"
+  }
+];
